7c9c825bd2789bcb5ac4607c1899bc0d7af8d97a
Gauransh9958 <gauransh9958@gmail.com>
2024-07-30 14:10:19 +0530

    login,noti,app.js added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
-import List from "./components/list/List"
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/Login/Login";
+import Notification from "./components/notification/notification";
 
 const App = () => {
+
+  const user = false;  
+
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
